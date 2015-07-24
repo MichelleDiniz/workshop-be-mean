0cdffed2bcff60215289e1f2057a848282be45bc
Miika <mfabianad@gmail.com>
2015-07-23 21:53:37 -0300

    sdfasdfadsf

--- node/aula-express/workshop-be-mean-online-julho-2015-aula-express-master/modules/beers/routes/api.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var Controller = require('./../controller');
-
-router.get('/', Controller.retrieve);
-
-router.post('/', Controller.create);
-
-module.exports = router;
